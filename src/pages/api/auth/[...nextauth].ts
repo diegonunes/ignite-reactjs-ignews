b7fbfa83a40655bb09c7957e81d0a6edeaa2a96a
Diego Nunes <diegonunesdev@gmail.com>
2022-08-12 01:03:41 -0300

    finished chapter VI

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
+import { query as q } from 'faunadb';
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -13,4 +15,23 @@ export default NextAuth({
       },
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email, name, following } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(q.Exists(q.Match(q.Index('users_by_email'), q.Casefold(user.email)))),
+            q.Create(q.Collection('users'), { data: { email } }),
+            q.Get(q.Match(q.Index('users_by_email'), q.Casefold(user.email)))
+          )
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
